121a11d34d53ecf847046ab5c2de31be649eab29
Johnson Kaho Poon <kahopoon@gmail.com>
2016-05-16 19:14:27 +0800

    pull to refresh implemented

--- ALPHACamp_finalAssessment_Q4/TableViewController.swift
+++ ALPHACamp_finalAssessment_Q4/TableViewController.swift
@@ -14,10 +14,11 @@ class TableViewController: UITableViewController {
 
     var metroInfo:[arrivedMetroInfo] = []
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        HUD.show(.LabeledProgress(title: "可愛的捷運資訊載入中。。。", subtitle: "稍候喔"))
+    func loadData() {
+        // remove cache in exist
+        metroInfo.removeAll()
+        print("test")
+        HUD.show(.LabeledProgress(title: "可愛的捷運資訊", subtitle: "正在可愛地載入中。。。"))
         metroDataAPICall { (result) in
             let allRows = result["result"]["results"].arrayValue
             for row in allRows {
@@ -29,24 +30,43 @@ class TableViewController: UITableViewController {
                 self.metroInfo.append(eachCuteCuteMetro)
             }
             self.tableView.reloadData()
+            self.refreshControl?.endRefreshing()
             HUD.hide()
         }
-
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // pull to refresh
+        self.refreshControl?.addTarget(self, action: #selector(TableViewController.loadData), forControlEvents: .ValueChanged)
+        self.refreshControl?.tintColor = UIColor.whiteColor()
+        
+        loadData()
     }
 
     func timeStandardToHumanReadable(nonFriendlyString: String) -> String {
         // this string can be like
         // 2016-05-16T18:00:51.953     or like
-        // 2016-05-16T18:00:51.95      or even
+        // 2016-05-16T18:00:51.95      or maybe
+        // 2016-05-16T18:00:51.9       or even
         // 2016-05-16T18:00:51         that will be nil when extracting, i love this api...
         let sourceString = nonFriendlyString
         let dateFormatter = NSDateFormatter()
         dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
-        dateFormatter.dateFormat = sourceString.characters.count == 23 ? "yyyy-MM-dd'T'HH:mm:ss.SSS" : (sourceString.characters.count == 22 ? "yyyy-MM-dd'T'HH:mm:ss.SS" : "yyyy-MM-dd'T'HH:mm:ss")
+        dateFormatter.dateFormat = sourceString.characters.count == 23 ? "yyyy-MM-dd'T'HH:mm:ss.SSS" : (sourceString.characters.count == 22 ? "yyyy-MM-dd'T'HH:mm:ss.SS" : (sourceString.characters.count == 21 ? "yyyy-MM-dd'T'HH:mm:ss.S" : "yyyy-MM-dd'T'HH:mm:ss"))
         let nsdate = dateFormatter.dateFromString(sourceString)
         let outputDateFormatter = NSDateFormatter()
         outputDateFormatter.dateFormat = "yyyy年MM月dd日HH時mm分"
         return outputDateFormatter.stringFromDate(nsdate!)
+    }
+    
+    func alertOfAnswer(answer: String) {
+        let showMessage = "真的是\(answer)耶!"
+        let alertController = UIAlertController(title: "你好聰明，竟然答對了！", message: showMessage, preferredStyle: .Alert)
+        let alertAction = UIAlertAction(title: "當然當然", style: .Cancel, handler: nil)
+        alertController.addAction(alertAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
@@ -68,14 +88,7 @@ class TableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        alertOfAnswer(metroInfo[indexPath.row].destination!)
     }
-    */
-
 }
