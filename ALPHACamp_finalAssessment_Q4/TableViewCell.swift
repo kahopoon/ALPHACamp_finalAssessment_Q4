8d7022ed0cb2a1f9858c92301264f2d27ff80077
Johnson Kaho Poon <kahopoon@gmail.com>
2016-05-16 17:09:44 +0800

    view controller replaced with tableview controller

---
+++ ALPHACamp_finalAssessment_Q4/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  ALPHACamp_finalAssessment_Q4
+//
+//  Created by Ka Ho on 16/5/2016.
+//  Copyright © 2016 Ka Ho. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
